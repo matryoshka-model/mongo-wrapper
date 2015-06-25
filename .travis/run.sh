8713dc2d45dfb938c1be5b7984241fc66a6346e4
Leonardo Di Donato <leodidonato@gmail.com>
2015-06-25 19:11:27 +0200

    TravisCI setup

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,7 +3,7 @@
 declare -a mongo_ext=("1.4.5" "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8")
 
 composer self-update
-
+composer config --global github-oauth.github.com ${GH_TOKEN}
 pecl uninstall mongo
 
 for version in "${mongo_ext[@]}"
@@ -11,7 +11,7 @@ do
     echo " Installing mongo-${version}"
     yes "no" | pecl install mongo-${version}
 
-    composer install --prefer-source
+    composer install
 
     vendor/bin/phpunit
 done
