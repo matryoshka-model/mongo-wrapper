0b8d4ec0c7d91aca1fc0e36586186c9fc7405e28
Leonardo Di Donato <leodidonato@gmail.com>
2015-06-26 00:59:40 +0200

    Restoring all mongo ext versions

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $HOME/logs
 
-declare -a mongo_ext=("1.4.5") # "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8" "1.6.0" "1.6.1" "1.6.2" "1.6.3" "1.6.4" "1.6.5" "1.6.6" "1.6.7" "1.6.8" "1.6.9")
+declare -a mongo_ext=("1.4.5" "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8" "1.6.0" "1.6.1" "1.6.2" "1.6.3" "1.6.4" "1.6.5" "1.6.6" "1.6.7" "1.6.8" "1.6.9")
 
 echo "> UPDATING: pecl"
 pecl channel-update pecl.php.net > $HOME/logs/common.log
@@ -13,10 +13,9 @@ for version in "${mongo_ext[@]}"
 do
     echo "> INSTALLING: mongo-ext ${version}"
     yes "no" | pecl install "mongo-${version}" > $HOME/logs/mong-${version}.log 2>&1
-    echo $?
 
     echo "> INSTALLING: dependencies"
-    composer install
+    composer install --quiet
     echo "> RUN: test against mongo-ext ${version}"
     vendor/bin/phpunit
 done
