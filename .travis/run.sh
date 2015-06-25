b76de2ad2401bedc34abc2e87e5550ee2a7afe34
Leonardo Di Donato <leodidonato@gmail.com>
2015-06-25 19:34:07 +0200

    Testing against mongo ext 1.6.* series (10 new versions)

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-declare -a mongo_ext=("1.4.5" "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8")
+declare -a mongo_ext=("1.4.5" "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8" "1.6.0" "1.6.1" "1.6.2" "1.6.3" "1.6.4" "1.6.5" "1.6.6" "1.6.7" "1.6.8" "1.6.9")
 
+echo "> UPDATING pecl"
 pecl channel-update pecl.php.net
+echo "> UNINSTALLING travis mongo"
 pecl uninstall mongo
 
 for version in "${mongo_ext[@]}"
 do
     echo "> INSTALLING: mongo-${version}"
-    yes "no" | pecl install mongo-${version}
+    (yes "no" | pecl install mongo-${version}) | cat - 1>&2 # redirect stdout to stderr
 
+    echo "> INSTALLING: dependencies"
     composer install
+    echo "> RUN: tests"
     vendor/bin/phpunit
 done
