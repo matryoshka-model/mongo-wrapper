b286d0ef0ffc6e15ae6f2800601b8c4b5d58f365
Leonardo Di Donato <leodidonato@gmail.com>
2015-06-25 19:48:08 +0200

    TravisCI run experiments

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,24 +2,22 @@
 
 set -eo pipefail;
 
-output_redirect() {
-    cat - 1>&2 # redirect stdout to stderr
-}
+mkdir -p $HOME/logs
 
-declare -a mongo_ext=("1.4.5" "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8" "1.6.0" "1.6.1" "1.6.2" "1.6.3" "1.6.4" "1.6.5" "1.6.6" "1.6.7" "1.6.8" "1.6.9")
+declare -a mongo_ext=("1.4.5") # "1.5.0" "1.5.1" "1.5.2" "1.5.3" "1.5.3" "1.5.5" "1.5.6" "1.5.7" "1.5.8" "1.6.0" "1.6.1" "1.6.2" "1.6.3" "1.6.4" "1.6.5" "1.6.6" "1.6.7" "1.6.8" "1.6.9")
 
 echo "> UPDATING: pecl"
-pecl channel-update pecl.php.net | output_redirect
+pecl channel-update pecl.php.net >> $HOME/logs/pecl.update.log
 echo "> UNINSTALLING: (travis-ci) mongo"
-pecl uninstall mongo | output_redirect
+pecl uninstall mongo >> $HOME/logs/mongo.travis.uninstall.log
 
 for version in "${mongo_ext[@]}"
 do
-    echo "> INSTALLING: mongo-${version}"
-    (yes "no" | pecl install mongo-${version}) | output_redirect
+    echo "> INSTALLING: mongo-ext ${version}"
+    yes "no" | pecl install mongo-${version} >> "$HOME/logs/mongo-${version}.install.log"
 
     echo "> INSTALLING: dependencies"
     composer install
-    echo "> RUN: tests"
+    echo "> RUN: test against mongo-ext ${version}"
     vendor/bin/phpunit
 done
